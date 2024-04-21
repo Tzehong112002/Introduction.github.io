5df0db57b7fb5b71dbc2d6943bd24d431a794a42
Tan Tze Hong <tzehong112002@gmail.com>
2024-04-21 16:21:15 +0800

    Version Add Project

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,12 +79,12 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
+ 
   for (let i = 0; i < filterItems.length; i++) {
+    const itemCategory = filterItems[i].dataset.category.toLowerCase(); // Define and initialize itemCategory inside the loop
 
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+
+    if (selectedValue === "all" || selectedValue === itemCategory) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
